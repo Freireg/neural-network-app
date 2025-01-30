2e10109a5b5c5c6f6be42380971ed17e941a4023
freireg <freireg1503@gmail.com>
2025-01-30 09:37:03 -0300

    Plot adjustments

--- Scripts/inferences-comparison.py
+++ Scripts/inferences-comparison.py
@@ -7,20 +7,20 @@ file_path = 'model_bp5_predictions_embedded.csv'
 data = pd.read_csv(file_path)
 
 # Extract the required columns
-# expected = data['Expected']
+expected = data['Expected']
 model_bp5 = data['model_bp5']
 embedded = data['embedded']
 
 # Plot the results
 plt.figure(figsize=(10, 6))
-# plt.plot(expected, label='Expected', marker='o')
+plt.plot(expected, label='Expected', marker='o')
 plt.plot(embedded, label='Model on MCU', marker='s')
 plt.plot(model_bp5, label='Model on desktop', marker='x')
 
 # Add labels and title
 plt.xlabel('Index')
 plt.ylabel('Values')
-plt.title('Comparison of inferences executed on PC and MCU')
+plt.title('Comparison between the real values and the predictions')
 plt.legend()
 # Save the plot to a file
 plt.savefig('comparison_plot.png')
